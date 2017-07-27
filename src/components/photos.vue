5570093e3521ca0ae7ffff243fe1c86dd72fb318
moulin-PC\moulin <shine@moulin-orange.com>
2017-07-27 18:59:29 +0800

    complete photo layout

--- src/components/photos.vue
+++ src/components/photos.vue
@@ -5,6 +5,7 @@ export default {
       return{
         photos: [],
         pagingNext: '',
+        expanded: [],
         sizeIndex: 5,    // specific the size of returned images by arr index
       }
   },
@@ -14,8 +15,7 @@ export default {
   methods: {
     getPhotos(){
         FB.api('/me/photos?limit=24&type=uploaded&fields=images,link,likes.summary(true).limit(0),comments.limit(100){comment_count}', response => {
-            this.photos = this.photos.concat(response.data);
-            this.pagingNext = response.paging.next;
+            this.setResponse(response);
         });
     },
     getPaging(){
@@ -28,25 +28,40 @@ export default {
             return res.json();
         })
         .then(json => {
-            this.photos = this.photos.concat(json.data);
-            this.pagingNext = json.paging.next;
+            this.setResponse(json);
         })
         .catch(error => {
             console.error(error);
         })
     },
-    // getComments(ids){
-    //     let str = ids.join();
-    //     FB.api(`/?ids=${ids}`, response => {
-    //         this.$set(this, 'photos', response.data);
-    //     });
-    // },
+    setResponse(res){
+        let arr = new Array(res.data.length).fill(false);
+        this.expanded = this.expanded.concat(arr);
+        this.photos = this.photos.concat(res.data);
+        this.pagingNext = res.paging.next;
+    },
     sumComment(commentsArr){
         let total = commentsArr.reduce((sum, obj) => {
             return sum + obj.comment_count;
         }, 0);
         return total;
-    }
+    },
+
+    expandDetail(index){
+        if(this.expanded[index]){
+            this.closeDetail(index);
+        }
+        else{
+            this.expanded.fill(false);
+            this.$set(this.expanded, index, true);
+        }
+    },
+    closeDetail(index){
+        this.$set(this.expanded, index, false);
+    },
+    getOffset(i){
+        return `${i * -100}%`;
+    },
   }
 
 }
@@ -54,18 +69,6 @@ export default {
 
 <style lang="scss">
 #photos{
-    .image{
-        width: 100%;
-        padding-top: 100%;
-        background-size: cover;
-        background-position: center center;
-    }
-    .data{
-        text-align: left;
-        font-size: 18px;
-        padding: 10px;
-        color: #333;
-    }
     .scale-enter-active, .scale-leave-active{
         transition: all .5s;
     }
@@ -73,6 +76,51 @@ export default {
         opacity: 0;
         transform: scale(.5);
     }
+    .photo__brief{
+        position: relative;
+        cursor: pointer;
+
+        .image{
+            width: 100%;
+            padding-top: 100%;
+            background-size: cover;
+            background-position: center center;
+        }
+        .data{
+            text-align: left;
+            font-size: 18px;
+            padding: 10px;
+            color: #333;
+        }
+    }
+
+    .arrow-up{
+        margin: 3px auto 0;
+        width: 0;
+        height: 0;
+        border-left: 10px solid transparent;
+        border-right: 10px solid transparent;
+        border-bottom: 10px solid #333;
+    }
+    .photo__detail{
+        position: relative;
+        width: 400%;
+        background-color: #333;
+        color: #fff;
+
+        .close-btn{
+            position: absolute;
+            right: 15px;
+            top: 15px;
+            cursor: pointer;
+            color: #999;
+            
+            &:hover{
+                color: #fff;
+            }
+        }
+    }
+
 }
 
 </style>
@@ -81,26 +129,42 @@ export default {
 <template>
     <div id="photos">
         <!-- <Row :gutter="16"> -->
-        <transition-group name="scale" tag="div" class="ivu-row"> 
-            <Col v-for="photo in photos" :key="photo.id" span="6" class="margin-bottom" style="padding: 0 8px;">
-                <a :href="photo.link" target="_blank">
-                    <Card :padding="0">
-                        <div class="image" :style="`background-image: url(${photo.images[sizeIndex].source});`"></div>
-                        <div class="data">
-                            <div><Icon type="thumbsup"></Icon> {{ photo.likes.summary.total_count }}</div>
-                            <div v-if="photo.comments">
-                            <!-- 不加 v-if 就會爆 -->
-                                <div>直接留言數: {{ photo.comments.data.length }}</div>
-                                <div>總留言數: {{ photo.comments.data.length + sumComment(photo.comments.data) }}</div>
+        <transition-group name="scale" tag="div" class="ivu-row ivu-row-flex ivu-row-flex-top"> 
+            <div class="ivu-col ivu-col-span-6 margin-bottom"
+                v-for="(photo, index) in photos" :key="photo.id">
+                <!-- <a :href="photo.link" target="_blank"> -->
+                    <div class="photo__brief" @click="expandDetail(index)" style="padding: 0 8px;">
+                        <Card :padding="0">
+                            <div class="image" :style="`background-image: url(${photo.images[sizeIndex].source});`"></div>
+                            <div class="data">
+                                <div><Icon type="thumbsup"></Icon> {{ photo.likes.summary.total_count }}</div>
+                                <div v-if="photo.comments">
+                                <!-- 不加 v-if 就會爆 -->
+                                    <div>直接留言數: {{ photo.comments.data.length }}</div>
+                                    <div>總留言數: {{ photo.comments.data.length + sumComment(photo.comments.data) }}</div>
+                                </div>
+                                <div v-else>
+                                    <div>直接留言數: 0</div>
+                                    <div>總留言數: 0</div>
+                                </div>
                             </div>
-                            <div v-else>
-                                <div>直接留言數: 0</div>
-                                <div>總留言數: 0</div>
-                            </div>
-                        </div>
-                    </Card>
-                </a>
-            </Col>                
+                        </Card>
+
+                        <div v-if="expanded[index]" class="arrow-up"></div>
+                    </div>
+                <!-- </a> -->
+                <div v-if="expanded[index]" class="photo__detail" :style="`margin-left: ${getOffset(index%4)};`">
+                    <div class="close-btn" @click="closeDetail(index)">
+                        <Icon type="close"></Icon>
+                    </div>
+                    <div>
+                        hey
+                        <br>
+                        hey
+                        <br>
+                    </div>
+                </div>
+            </div>                
         </transition-group> 
         <!-- </Row> -->
         <Row class="margin-bottom">
